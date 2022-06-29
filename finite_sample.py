2d0f83aefb8a3fa420c44ac2dc355e975d6c554f
naga <naga@nagacorp.com>
2022-06-29 17:53:01 +0530

    librosa delete

--- finite_sample.py
+++ finite_sample.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from keras.models import load_model
-import numpy as np
-import librosa
+#import numpy as np
+#import librosa
 import librosa.display
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
