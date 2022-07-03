8a62f10d4767a57e032924820ab56c039ab88a11
naga <naga@nagacorp.com>
2022-07-03 23:17:49 +0530

    title change

--- finite_sample.py
+++ finite_sample.py
@@ -183,7 +183,7 @@ sns.set()
 
 from PIL import Image
 
-#st.title('Audio Classifier')
+st.title('Audio Classifier')
 
 uploaded_file = st.file_uploader("Upload audio")
 
@@ -277,7 +277,7 @@ if uploaded_file is not None:
          st.title("Pricing Points Explanation")
 
          daily_price_res = predict_daily_price(featuesAll)
-         st.markdown("daily price explained: "+boldTag(daily_price_res[0])+ daily_price_res[0]+"</b> Confidence: <b>"+str(daily_price_res[1]*100)+"</b>", unsafe_allow_html=True)
+         st.markdown("Daily price explained: "+boldTag(daily_price_res[0])+ daily_price_res[0]+"</b> Confidence: <b>"+str(daily_price_res[1]*100)+"</b>", unsafe_allow_html=True)
          
          monthly_price_res = predict_monthly_price(featuesAll)
          st.markdown("monthly price explained: "+boldTag(monthly_price_res[0])+ monthly_price_res[0]+"</b> Confidence: <b>"+str(monthly_price_res[1]*100)+"</b>", unsafe_allow_html=True)
